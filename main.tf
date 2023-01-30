55406a45628bd9c30f663ebfb6d9783d98861719
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-01-30 17:25:03 -0500

    jammy

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "name"
-    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-impish-22.04-amd64-minimal*"]
+    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-jammy-22.04-amd64-minimal*"]
   }
 
   filter {
