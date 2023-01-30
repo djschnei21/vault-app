91d3fcc2c8851e125867611b63b25f54459d2d1b
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-01-30 16:46:20 -0500

    data lookups

--- main.tf
+++ main.tf
@@ -15,7 +15,6 @@ provider "doormat" {}
 
 data "doormat_aws_credentials" "creds" {
   provider = doormat
-
   role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role"
 }
 
@@ -23,4 +22,62 @@ provider "aws" {
   access_key = data.doormat_aws_credentials.creds.access_key
   secret_key = data.doormat_aws_credentials.creds.secret_key
   token      = data.doormat_aws_credentials.creds.token
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["679593333241"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu-minimal/images/hvm-ssd/ubuntu-focal-20.04-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+data "aws_vpc" "selected" {
+  filter {
+    name = "djs-lab-vpc"
+  }
+}
+
+data "aws_subnet_ids" "private" {
+  vpc_id = data.aws_vpc.selected.id
+
+  filter {
+    name = "djs-lab-subnet-private*"
+  }
+}
+
+data "aws_subnet_ids" "public" {
+  vpc_id = data.aws_vpc.selected.id
+
+  filter {
+    name = "djs-lab-subnet-public*"
+  }
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  for_each = toset(["1", "2", "3"])
+
+  name = "vault-${each.key}"
+
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t3.large"
+  key_name               = "djs-lab"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-028d1fce460391d4c"]
+  subnet_id              = "${element(data.aws_subnet_ids.private.ids, count.index)}"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
